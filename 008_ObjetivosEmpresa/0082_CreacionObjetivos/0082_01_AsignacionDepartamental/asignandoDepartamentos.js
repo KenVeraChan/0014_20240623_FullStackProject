ee2f0bef12518388f97002bfe3c36b7f0f298734
RasselinWR <sfer4d_corporation@outlook.com>
2025-01-16 15:01:43 +0100

    Falta operaciones de asignacion de roles a JEFES, RRHH, EMPLEADOS. Falta Cookies

--- 008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/asignandoDepartamentos.js
+++ 008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/asignandoDepartamentos.js
@@ -60,47 +60,11 @@ function cargarPagina()
         entradaVOLVER.style.transitionDuration="1.9s";
         entradaVOLVER.style.marginLeft= "10%";
     }
-    if(window.screen.width>481 && window.screen.width<=555)
+    if(window.screen.width>100 && window.screen.width<=555)
     {
         var entrada=document.getElementById("tablaBotones");
         entrada.style.transitionDuration="1.3s";
         entrada.style.marginLeft= "10%";
-                
-        var entradaJEFES=document.getElementById("eleccionesJEFE");
-        entradaJEFES.style.transitionDuration="1.3s";
-        entradaJEFES.style.marginLeft= "10%";
-    
-        var entradaRRHH=document.getElementById("eleccionesRRHH");
-        entradaRRHH.style.transitionDuration="1.6s";
-        entradaRRHH.style.marginLeft= "10%";
-    
-        var entradaVOLVER=document.getElementById("eleccionesEMPLEADO");
-        entradaVOLVER.style.transitionDuration="1.9s";
-        entradaVOLVER.style.marginLeft= "10%";
-    }
-    if(window.screen.width>300 && window.screen.width<=481)
-    {
-        var entrada=document.getElementById("tablaBotones");
-        entrada.style.transitionDuration="1.3s";
-        entrada.style.marginLeft= "15%";
-                
-        var entradaJEFES=document.getElementById("eleccionesJEFE");
-        entradaJEFES.style.transitionDuration="1.3s";
-        entradaJEFES.style.marginLeft= "10%";
-    
-        var entradaRRHH=document.getElementById("eleccionesRRHH");
-        entradaRRHH.style.transitionDuration="1.6s";
-        entradaRRHH.style.marginLeft= "10%";
-    
-        var entradaVOLVER=document.getElementById("eleccionesEMPLEADO");
-        entradaVOLVER.style.transitionDuration="1.9s";
-        entradaVOLVER.style.marginLeft= "10%";
-    }
-    if(window.screen.width>100 && window.screen.width<=300)
-    {
-        var entrada=document.getElementById("tablaBotones");
-        entrada.style.transitionDuration="1.3s";
-        entrada.style.marginLeft= "7%";
                 
         var entradaJEFES=document.getElementById("eleccionesJEFE");
         entradaJEFES.style.transitionDuration="1.3s";
@@ -147,4 +111,98 @@ function cargarPagina()
         volver.style.transitionDuration="0.25s";
         volver.style.boxShadow="none";
     })
+}
+function cargaFormularios(indice)
+{
+    var JEFES=document.getElementsByClassName("consultaJEFES")[0];
+    var RRHH=document.getElementsByClassName("consultaRRHH")[0];
+    var EMPLEADO=document.getElementsByClassName("consultaEMPLEADOS")[0];
+
+    if(indice==1)
+    {
+    //CARGA FORMULARIO JEFES
+        //SE RETIRAN TODOS LOS FORMULARIOS DE RR.HH. Y DE EMPLEADOS
+        RRHH.style.transitionDuration="0.3s";
+        RRHH.style.marginLeft= "-1200%";
+        RRHH.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        RRHH.style.boxShadow="none";
+        document.getElementById("eleccionesRRHH").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesRRHH").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesRRHH").style.textShadow = "none";
+
+        EMPLEADO.style.transitionDuration="0.5s";
+        EMPLEADO.style.marginLeft= "-1200%";
+        EMPLEADO.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        EMPLEADO.style.boxShadow="none";
+        document.getElementById("eleccionesEMPLEADO").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesEMPLEADO").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesEMPLEADO").style.textShadow = "none";
+
+        //SE COLOCA EN PANTALLA EL FORMULARIO DE JEFES
+        JEFES.style.transitionDuration="0.7s";
+        JEFES.style.marginLeft= "13.5%";
+        JEFES.style.backgroundColor="rgba(0, 78, 5, 0.85)";
+        JEFES.style.boxShadow="18px 18px 18px white";
+        document.getElementById("eleccionesJEFE").style.backgroundColor="rgba(0, 78, 5, 0.85)";
+        document.getElementById("eleccionesJEFE").style.color="rgba(115, 255, 0, 0.85)";
+        document.getElementById("eleccionesJEFE").style.textShadow = "20px 20px 1px rgba(255, 255, 255, 0.65)"
+
+    }
+    if(indice==2)
+    {
+    //CARGA FORMULARIO RRHH
+        //SE RETIRAN TODOS LOS FORMULARIOS DE RR.HH. Y DE EMPLEADOS
+        JEFES.style.transitionDuration="0.3s";
+        JEFES.style.marginLeft= "-1200%";
+        JEFES.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        JEFES.style.boxShadow="18px 18px 18px white";
+        document.getElementById("eleccionesJEFE").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesJEFE").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesJEFE").style.textShadow = "none";
+
+        EMPLEADO.style.transitionDuration="0.5s";
+        EMPLEADO.style.marginLeft= "-1200%";
+        EMPLEADO.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        EMPLEADO.style.boxShadow="none";
+        document.getElementById("eleccionesEMPLEADO").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesEMPLEADO").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesEMPLEADO").style.textShadow = "none";
+        //SE COLOCA EN PANTALLA EL FORMULARIO DE JEFES
+        RRHH.style.transitionDuration="0.7s";
+        RRHH.style.marginLeft= "13.5%";
+        RRHH.style.backgroundColor="rgba(77, 75, 0, 0.85)";
+        RRHH.style.boxShadow="18px 18px 18px white";
+        document.getElementById("eleccionesRRHH").style.backgroundColor="rgba(77, 75, 0, 0.85)";
+        document.getElementById("eleccionesRRHH").style.color="rgba(255, 247, 0, 0.85)";
+        document.getElementById("eleccionesRRHH").style.textShadow = "20px 20px 1px rgba(255, 255, 255, 0.65)";
+    }
+    if(indice==3)
+    {
+    //CARGA FORMULARIO EMPLEADOS
+        //SE RETIRAN TODOS LOS FORMULARIOS DE RR.HH. Y DE EMPLEADOS
+        JEFES.style.transitionDuration="0.3s";
+        JEFES.style.marginLeft= "-1200%";
+        JEFES.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        JEFES.style.boxShadow="18px 18px 18px white";
+        document.getElementById("eleccionesJEFE").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesJEFE").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesJEFE").style.textShadow = "none";
+
+        RRHH.style.transitionDuration="0.5s";
+        RRHH.style.marginLeft= "-1200%";
+        RRHH.style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        RRHH.style.boxShadow="none";
+        document.getElementById("eleccionesRRHH").style.backgroundColor="rgba(0, 0, 19, 0.89)";
+        document.getElementById("eleccionesRRHH").style.color="rgb(255, 255, 255)";
+        document.getElementById("eleccionesRRHH").style.textShadow = "none";
+
+        //SE COLOCA EN PANTALLA EL FORMULARIO DE JEFES
+        EMPLEADO.style.transitionDuration="0.7s";
+        EMPLEADO.style.marginLeft= "13.5%";
+        EMPLEADO.style.backgroundColor="rgba(92, 0, 0, 0.85)";
+        EMPLEADO.style.boxShadow="18px 18px 18px white";
+        document.getElementById("eleccionesEMPLEADO").style.backgroundColor="rgba(92, 0, 0, 0.85)";
+        document.getElementById("eleccionesEMPLEADO").style.color="rgba(255, 0, 0, 0.85)";
+        document.getElementById("eleccionesEMPLEADO").style.textShadow = "20px 20px 1px rgba(255, 255, 255, 0.65)";
+    }
 }
--- 008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/asignandoDepartamentos.php
+++ 008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/asignandoDepartamentos.php
@@ -22,19 +22,80 @@
         <table id="tablaBotones">
             <tr class="filaBoton">
                 <td>
-                    <div id="eleccionesJEFE" onclick="location.href='../../005_Login/0052_LoginJEFES/loginJEFES.php'">JEFES</div>
+                    <div id="eleccionesJEFE" onclick="cargaFormularios(1)">JEFES</div>
                 </td>
                 <td>
-                    <div id="eleccionesRRHH" onclick="location.href='../../005_Login/0051_LoginRRHH/loginRRHH.php'">RR.HH.</div>
+                    <div id="eleccionesRRHH" onclick="cargaFormularios(2)">RR.HH.</div>
                 </td>
                 <td>
-                    <div id="eleccionesEMPLEADO" onclick="location.href='../../005_Login/0051_LoginRRHH/loginRRHH.php'">EMPLEADOS</div>
+                    <div id="eleccionesEMPLEADO" onclick="cargaFormularios(3)">EMPLEADOS</div>
                 </td>
             </tr>
         </table>
         <div id="iconoAdorno"><img class="imagenIcono" src="../../../008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/images/Sfer4D-IconoEmpresa.jpg" alt="iconoEmpresa"></div>     
         <div class="VaciobotonesPrincipal"></div>
         <img id="imagenPortada" src="../../../008_ObjetivosEmpresa/0082_CreacionObjetivos/0082_01_AsignacionDepartamental/images/DEPARTAMENTOS.png" alt="Imagen departamentos">    
+        <div class="consultaJEFES">
+            <!-- INSERCCIONES TABLA 1: JEFES -->
+            <form class="tablaAcciones" action="../../../008_ObjetivosEmpresa/0082_CreacionObjetivos/gestionTareas.php" method="POST">
+                <p class="separacion"></p>
+                    <label class="celdaJEFES">DEPARTAMENTO ASIGNADO:
+                        <select name="rolJEFES" class="desplegableJEFES">
+                            <option></option>
+                            <option>EJECUTIVO</option>
+                            <option>OPERACIONES</option>
+                            <option>FINANCIERO</option>
+                            <option>I+D+I</option>
+                            <option>MARKETING</option>
+                            <option>CONSULTOR</option>
+                            <option>PRODUCCION</option>
+                            <option>DATOS</option>
+                        </select>
+                    </label>
+                <p class="separacion"></p>
+                <label class="celdaJEFES">CONTRASEÑA ASIGNADA:<input type="text" class="celdasJEFES" name="contraseniaAsignada" required minlength="4" maxlength="28"></label> <!--CONTRASENIA ASIGNADA-->
+                <p class="separacion"></p>
+            </form>
+        </div>
+        <div class="consultaRRHH">
+            <!-- INSERCCIONES TABLA 2: RRHH -->
+            <form class="tablaAcciones" action="../../../008_ObjetivosEmpresa/0082_CreacionObjetivos/gestionTareas.php" method="POST">
+                <p class="separacion"></p>
+                    <label class="celdaRRHH">DEPARTAMENTO:
+                        <select name="rolRRHH" class="desplegableRRHH">
+                            <option></option>
+                            <option>RRHH</option>
+                            <option>RELACIONES PUBLICAS</option>
+                            <option>DIRECTOR</option>
+                        </select>
+                    </label>
+                <p class="separacion"></p>
+                <label class="celdaRRHH">CONTRASEÑA ASIGNADA:<input type="text" class="celdasRRHH" name="contraseniaAsignada" required minlength="4" maxlength="28"></label> <!--CONTRASENIA ASIGNADA-->
+                <p class="separacion"></p>
+            </form>
+        </div>
+        <div class="consultaEMPLEADOS">
+            <!-- INSERCCIONES TABLA 3: EMPLEADOS -->
+            <form class="tablaAcciones" action="../../../008_ObjetivosEmpresa/0082_CreacionObjetivos/gestionTareas.php" method="POST">
+                <p class="separacion"></p>
+                    <label class="celdaEMPLEADOS">CONTRATO ESTABLECIDO:
+                        <select name="rolEMPLEADOS" class="desplegableEMPLEADOS">
+                            <option></option>
+                            <option>TIEMPO COMPLETO</option>
+                            <option>INDEFINIDO</option>
+                            <option>OBRA Y OFICIO</option>
+                            <option>SUSTITUCIÓN</option>
+                            <option>ESTACIONAL</option>
+                            <option>VOLUNTARIO</option>
+                            <option>ARRENDADOS</option>
+                            <option>PRACTICANTE</option>
+                        </select>
+                    </label>
+                <p class="separacion"></p>
+                <label class="celdaEMPLEADOS">CONTRASEÑA ASIGNADA:<input type="text" class="celdasEMPLEADOS" name="contraseniaAsignada" required minlength="4" maxlength="28"></label> <!--CONTRASENIA ASIGNADA-->
+                <p class="separacion"></p>
+            </form>
+        </div>
     </header>
     <div class="piePagina">
         <footer id="piePrincipal">
